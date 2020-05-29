67f611eab3977c9f683de279e7e04d03aa019366
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-05-29 12:44:02 +0200

    layout main

--- app/src/main/java/com/example/pickup/AdressFragment.kt
+++ app/src/main/java/com/example/pickup/AdressFragment.kt
@@ -1,5 +1,6 @@
 package com.example.pickup
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,23 +10,24 @@ import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class AdressFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_adress, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        view.findViewById<Button>(R.id.btSearch).setOnClickListener {
+            val intent = Intent(activity, PackageActivity::class.java)
+            activity?.startActivity(intent)
         }
     }
 }
