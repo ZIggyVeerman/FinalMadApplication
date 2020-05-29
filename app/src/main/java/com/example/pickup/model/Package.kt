2078afe07b943afc6e8ef480d71b752cf77b721a
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-05-29 11:08:57 +0200

    dependencies

---
+++ app/src/main/java/com/example/pickup/model/Package.kt
@@ -0,0 +1,9 @@
+package com.example.pickup.model
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+//@Parcelize
+//data class MovieItem(
+//    @SerializedName("")
+//)
